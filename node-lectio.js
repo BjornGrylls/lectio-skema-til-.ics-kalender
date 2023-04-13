f432f9b00ae306040a83939c93633cf00997478c
BjornGrylls <bjornskole@gmail.com>
2023-04-13 13:05:59 +0200

    Created list to skip duplicate events

--- node-lectio.js
+++ node-lectio.js
@@ -361,18 +361,25 @@ function lectio(res, amount, type, school, person, skew) {
                     var cheerio = require('cheerio');
                     $ = cheerio.load(body);
                     l.els = l.els + $('.s2bgbox')['length'];
+                    var absids = [];
                     $('.s2bgbox').each(function(i, item) {
-                        var promise = new Promise(function(resolve, reject) {
-                            l.processEvent(
-                                item.attribs['data-additionalinfo'],
-                                function(output) {
-                                    resolve(output);
-                                },
-                            );
-                        });
-                        promise.then(function(output) {
-                            l.res.write(output);
-                        });
+                        var regExp = '/absid=(\\d+)/'; // Create regex
+                        var absid = regExp.exec(item.attribs['href'])[1].split('=')[1]; // Find absid
+                        if (!absids.includes(absid)) { // Check absid
+                            absids.push(absid); // Add absid
+
+                            var promise = new Promise(function(resolve, reject) {
+                                l.processEvent(
+                                    item.attribs['data-additionalinfo'],
+                                    function(output) {
+                                        resolve(output);
+                                    },
+                                );
+                            });
+                            promise.then(function(output) {
+                                l.res.write(output);
+                            });
+                        }
                     });
                 })
                 .catch(function(error) {
